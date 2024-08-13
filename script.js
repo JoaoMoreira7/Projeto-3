2b191375446d886980d680c3648d62076474e080
joaomoreirapj <peritojoaomoreira@gmail.com>
2024-08-13 19:05:36 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector{'.busca'}.addEventListstener('.busca').addEventListener('submit',(event)=>{
+   event.preventDefaul();
+   let input = document.querySelector('#searchInput').value;
+})
